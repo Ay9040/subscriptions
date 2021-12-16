46fcb8a30b33056a865ad07896b13600b8c389c4
Ayush Khade <ayush.khade04@outlook.com>
2021-12-16 12:39:37 +0530

    added singup page file

--- src/App.js
+++ src/App.js
@@ -7,14 +7,15 @@ import React from 'react';
 import axios from 'axios';
 import background from "./header.png";
 import Home from './Home'
-function checkLogin(){
+import Signup from './Signup'
+function checkLogin() {
 
 }
 
 function App() {
   const [login, setLogin] = useState(null)
 
-  useEffect(async function() {
+  useEffect(async function () {
     const loggedInUser = await localStorage.getItem("user");
     console.log(loggedInUser)
     if (loggedInUser) {
@@ -26,14 +27,15 @@ function App() {
 
   return (
     <Router>
-    <div className="App min-h-screen" style={{backgroundImage: `url(${background})`}}>
-      <Navbar login={login}/>
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/subscriptions" component={() => login == null ? <Login login={login} setLogin={setLogin} message={null}/> : <Subscription username={login}/>} />
-      </Switch>
-    </div>
-    </Router> 
+      <div className="App min-h-screen" style={{ backgroundImage: `url(${background})` }}>
+        <Navbar login={login} />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/subscriptions" component={() => login == null ? <Login login={login} setLogin={setLogin} message={null} /> : <Subscription username={login} />} />
+          <Route path="/signup" component={Signup} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- src/Signup.js
+++ src/Signup.js
@@ -1,38 +1,42 @@
 import axios from 'axios'
 import { useState } from 'react';
 import React from 'react';
-import { Redirect } from 'react-router-dom';
-async function handleSubmit(e, setShowModal) {
-    e.preventDefault()
-    window.location.href = '/signup'
-    return
-}
 
-async function handleLogin(e, setLogin, setShowModal) {
+async function handleSubmit(e, setShowModal) {
+    console.log("Signup called")
+    var error = false
     e.preventDefault()
-    let res;
+    var res = {};
     var username = (e.target.form[0].value)
     var passwd = e.target.form[1].value
-    await axios.get("https://subscription-back.herokuapp.com/api/users/" + username).then((data) => {
+    await axios.get("https://subscription-back.herokuapp.com/api/users/" + username).then(async function (data) {
         res = data['data']
-        if (username == res['username'] && passwd == res['passwd']) {
-            console.log(res)
-            setLogin(res)
-            localStorage.setItem('user', JSON.stringify(res))
-        }
-        else {
-            setShowModal("Wrong Username/Password Combination")
+        if (res != null && res['username'] == username) {
+            console.log("User already exists")
+            setShowModal("User already exists")
+            error = true
             setTimeout(() => { setShowModal(false) }, 2700)
-            console.log("invalid credentails")
         }
+        await axios.post("https://subscription-back.herokuapp.com/api/users", {
+            "username": username,
+            "passwd": passwd
+        }).catch(e => {
+            setShowModal("Error, Please try again after sometime")
+            error = true
+            setTimeout(() => { setShowModal(false) }, 2700)
+        })
     }).catch(e => {
+        console.log(e)
         setShowModal("Error, Please try again after sometime")
+        error = true
         setTimeout(() => { setShowModal(false) }, 2700)
-        return
     })
+    if (!error)
+        window.location.href = "/subscriptions"
 }
 
-function Login(props) {
+
+function Signup(props) {
     const [showModal, setShowModal] = useState(false)
     console.log(props)
     return (
@@ -54,7 +58,7 @@ function Login(props) {
                 </>
             ) : null}
             <div className="flex flex-col">
-                <div className="text-4xl text-white m-10">Login</div>
+                <div className="text-4xl text-white m-10">Sign up</div>
                 <form>
                     <div className="flex flex-row">
                         <div className="flex flex-col">
@@ -66,7 +70,6 @@ function Login(props) {
                             <input className="p-2 border-grey-200 border-2 rounded-md focus:outline-none mt-5 w-96" type="password" ></input>
                         </div>
                     </div>
-                    <input className="m-auto my-5  p-3 transition duration-500 cursor-pointer rounded-lg w-32 ease-in-out text-white bg-green-400 hover:bg-green-700 hover:text-white focus:outline-none transform hover:scale-100" value="Login" type="submit" onClick={(e) => handleLogin(e, props.setLogin, setShowModal)}></input>
                     <input className="m-auto my-5 ml-4 p-3 transition duration-500 cursor-pointer rounded-lg w-32 ease-in-out text-white bg-blue-400 hover:bg-blue-700 hover:text-white focus:outline-none transform hover:scale-100" value="Sign Up" type="submit" onClick={(e) => handleSubmit(e, setShowModal)}></input>
                 </form>
             </div>
@@ -74,4 +77,4 @@ function Login(props) {
     )
 }
 
-export default Login
+export default Signup
